c6adecc752b447a490572091fcd3d715b4555f14
Julio_Leme <julioromao3@gmail.com>
2019-04-10 18:30:46 -0300

    Versao 1.2 - criacao de model

---
+++ src/main/java/com/uol/model/ClienteVO.java
@@ -0,0 +1,50 @@
+package com.uol.model;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author julio
+ * @since 2019-03-26
+ * @version 1.0.0
+ * 
+ */
+public class ClienteVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String nome;
+	private Integer idade;
+
+	/**
+	 * getters and setters
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+}
